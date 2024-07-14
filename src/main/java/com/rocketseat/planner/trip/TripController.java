632aed1dcaee02395ae69535045883f090a2a405
debora <deborapatriciadp27@gmail.com>
2024-07-14 20:59:43 -0300

    feat: Adiciona métodos GET para consulta de viagens
    - Adiciona o método getTripById no TripController para buscar uma viagem pelo seu ID.
    - Adiciona o método getAllTrips no TripController para buscar todas as viagens.
    - Utiliza o TripRepository para realizar as consultas no banco de dados.
    
    Essas adições permitem consultar informações específicas de viagens e listar todas as viagens disponíveis.

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -1,7 +1,13 @@
 package com.rocketseat.planner.trip;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +33,21 @@ public class TripController {
     this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
     return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Trip> getTripById(@PathVariable UUID id) {
+    Optional<Trip> trip = repository.findById(id);
+    if (trip.isPresent()) {
+      return ResponseEntity.ok(trip.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Trip>> getAllTrips() {
+    List<Trip> trips = repository.findAll();
+    return ResponseEntity.ok(trips);
   }
 }
